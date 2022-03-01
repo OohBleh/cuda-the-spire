b7739abb18d5577aaa38fb4fe8985bfc464d9660
OohBleh <76603321+OohBleh@users.noreply.github.com>
2022-02-28 23:19:30 -0500

    tidying Neow filter and card filter

--- sts.cu
+++ sts.cu
@@ -145,10 +145,10 @@ __global__ void pandoraSeedKernelFast(TestInfo info, bool* results) {
 
 
 template<uint8 nCardRewards>
-__forceinline__ __device__ bool testBadNeow(const uint64 seed) {
+__forceinline__ __device__ bool testBadNeow1(const uint64 seed) {
 	uint64 seed0 = murmurHash3(seed);
 	uint64 seed1 = murmurHash3(seed0);
-	
+
 	/*
 		[ ] THREE_CARDS = 0,
 		[ ] ONE_RANDOM_RARE_CARD,
@@ -161,114 +161,105 @@ __forceinline__ __device__ bool testBadNeow(const uint64 seed) {
 	if (k != 2) {
 		return false;
 	}
-	
+
 	/*
 		[ ] THREE_SMALL_POTIONS,
-        [x] RANDOM_COMMON_RELIC,
-        [x] TEN_PERCENT_HP_BONUS,
-        [ ] THREE_ENEMY_KILL,
-        [x] HUNDRED_GOLD,
+		[x] RANDOM_COMMON_RELIC,
+		[x] TEN_PERCENT_HP_BONUS,
+		[ ] THREE_ENEMY_KILL,
+		[x] HUNDRED_GOLD,
 	*/
 	k = random64Fast<5>(seed0, seed1);
 	//if ((k != 1) && (k != 4)) {
 	//if (k != 4) {
-	if ((k == 0) || (k == 3)){
+	if ((k == 0) || (k == 3)) {
 		return false;
 	}
 	k = random64Fast<4>(seed0, seed1);
-	/*
-		0 TEN_PERCENT_HP_LOSS:
-			0 [ ] RANDOM_COLORLESS_2,
-            1 [?] REMOVE_TWO,
-            2 [ ] ONE_RARE_RELIC,
-            3 [ ] THREE_RARE_CARDS,
-            4 [x] TWO_FIFTY_GOLD,
-            5 [ ] TRANSFORM_TWO_CARDS,
-		1 NO_GOLD:
-			0 [ ] RANDOM_COLORLESS_2,
-            1 [?] REMOVE_TWO,
-            2 [ ] ONE_RARE_RELIC,
-            3 [ ] THREE_RARE_CARDS,
-            4 [ ] TRANSFORM_TWO_CARDS,
-            5 [x] TWENTY_PERCENT_HP_BONUS,
-		2 CURSE:
-			0 [ ] RANDOM_COLORLESS_2,
-            1 [ ] ONE_RARE_RELIC,
-            2 [ ] THREE_RARE_CARDS,
-            3 [x] TWO_FIFTY_GOLD,
-            4 [ ] TRANSFORM_TWO_CARDS,
-            5 [x] TWENTY_PERCENT_HP_BONUS,
-		3 PERCENT_DAMAGE:
-			0 [ ] RANDOM_COLORLESS_2,
-			1 [?] REMOVE_TWO,
-			2 [ ] ONE_RARE_RELIC,
-			3 [ ] THREE_RARE_CARDS,
-			4 [x] TWO_FIFTY_GOLD,
-			5 [ ] TRANSFORM_TWO_CARDS,
-			6 [x] TWENTY_PERCENT_HP_BONUS,
+	/*										0 HP_LOSS	1 NO_GOLD	2 CURSE		3 DAMAGE
+			0 [ ] RANDOM_COLORLESS_2,			0			0			0			0
+			1 [ ] REMOVE_TWO,					1			1			=====		1
+			2 [ ] ONE_RARE_RELIC,				2			2			1			2
+			3 [ ] THREE_RARE_CARDS,				3			3			2			3
+			4 [x] TWO_FIFTY_GOLD,				[4]			=====		[3]			[4]
+			5 [ ] TRANSFORM_TWO_CARDS,			5			4			4			5
+			6 [x] TWENTY_PERCENT_HP_BONUS,		=====		[5]			[5]			[6]
 	*/
-	
+
 	uint8 l = (k == 3) ? random64Fast<7>(seed0, seed1) : random64Fast<6>(seed0, seed1);
 	l = (k == 1) ? (l - 1) : l;
 	l = (k == 2) ? (l + 1) : l;
 	//l = (k == 1) ? 0 : l;
 	return (l == 4) || (l == 6);
 
-	//uint8 l = random64Fast<42>(seed0, seed1);
-	//l = (k == 2) ? (l + 1) : l;
-	//l = (k == 3) ? (l % 7) : (l % 6);
-	//return (l == 4) || (k == 1);
-
-	switch (k) {
-	case 0:
-		return random64Fast<6>(seed0, seed1) == 4;
-		/*
-		if (random64Fast<6>(seed0, seed1) != 4) {
-			return false;
-		}
-		*/
-	case 1:
-		return false;
-	case 2:
-		return random64Fast<6>(seed0, seed1) == 3;
-		/*
-		if (random64Fast<6>(seed0, seed1) != 3) {
-			return false;
-		}
-		*/
-	case 3:
-		return random64Fast<7>(seed0, seed1) == 4;
-		/*
-		if (random64Fast<7>(seed0, seed1) != 4) {
-			return false;
-		}
-		*/
-
-	}
-
 	return true;
 }
 
 
+template<uint8 nCardRewards>
+__forceinline__ __device__ bool testBadNeow2(const uint64 seed) {
+	uint64 seed0 = murmurHash3(seed);
+	uint64 seed1 = murmurHash3(seed0);
 
+	/*
+		[ ] 0 THREE_CARDS = 0,
+		[ ] 1 ONE_RANDOM_RARE_CARD,
+		[ ] 2 REMOVE_CARD,
+		[ ] 3 UPGRADE_CARD,
+		[x] 4 TRANSFORM_CARD,
+		[ ] 5 RANDOM_COLORLESS,
+	*/
+	
+	if (random64Fast<6>(seed0, seed1) != 4) {
+		return false;
+	}
 
+	/*
+		[ ] THREE_SMALL_POTIONS,
+		[x] RANDOM_COMMON_RELIC,
+		[x] TEN_PERCENT_HP_BONUS,
+		[ ] THREE_ENEMY_KILL,
+		[x] HUNDRED_GOLD,
+	*/
+	uint8 k = random64Fast<5>(seed0, seed1);
+	if ((k == 0) || (k == 3)) {
+		return false;
+	}
+	k = random64Fast<4>(seed0, seed1);
+	/*									0 HP_LOSS	1 NO_GOLD	2 CURSE		3 DAMAGE
+		[ ] RANDOM_COLORLESS_2,				0			0			0			0
+		[ ] REMOVE_TWO,						1			1			=====		1
+		[ ] ONE_RARE_RELIC,					2			2			1			2
+		[ ] THREE_RARE_CARDS,				3			3			2			3
+		[x] TWO_FIFTY_GOLD,					[4]			=====		[3]			[4]
+		[ ] TRANSFORM_TWO_CARDS,			5			4			4			5
+		[x] TWENTY_PERCENT_HP_BONUS,		=====		[5]			[5]			[6]
+	*/
 
+	uint8 l = (k == 3) ? random64Fast<7>(seed0, seed1) : random64Fast<6>(seed0, seed1);
+	l = (k == 1) ? (l - 1) : l;
+	l = (k == 2) ? (l + 1) : l;
+	//l = (k == 1) ? 0 : l;
+	return (l == 4) || (l == 6);
 
-
-
-
-
+	return true;
+}
 
 template<uint8 nCardRewards>
 __forceinline__ __device__ bool testBadSilentCardsFast(const uint64 seed) {
 
-	/*
-	uint64 seed0 = seed;
-	uint64 seed1 = murmurHash3(seed0);
-	*/
-
 	uint64 seed0 = murmurHash3(seed);
 	uint64 seed1 = murmurHash3(seed0);
+
+	constexpr uint64 BAD_COMMON = 188480;
+	constexpr uint64 BAD_UNCOMMON = 1912611214;
+	constexpr uint64 BAD_RARE = 67120;
+
+	constexpr uint16 NUM_A = 19;
+	constexpr uint16 NUM_B = 33;
+	constexpr uint16 NUM_AB = 52;
+	//constexpr uint16 NUM_C = 19;
+	constexpr uint16 NUM_GCD = 627;
 
 	uint8 adj = 5;
 
@@ -282,66 +273,26 @@ __forceinline__ __device__ bool testBadSilentCardsFast(const uint64 seed) {
 		bool is_rare = (roll < 3);
 		bool is_common = (roll >= 40);
 		bool is_uncommon = (!is_common) && (!is_rare);
-		uint16 card_roll = random64Fast<627>(seed0, seed1);
-		uint8 card_roll_mod19 = card_roll % 19;
-		uint8 card_roll_mod33 = card_roll % 33;
+		uint16 card_roll = random64Fast<NUM_GCD>(seed0, seed1);
+		uint8 card_roll_modA = card_roll % NUM_A;
+		uint8 card_roll_modB = card_roll % NUM_B;
 
-		bool is_reject = (is_common) && (((1 << card_roll_mod19) & 188480) == 0);
-		is_reject |= is_uncommon && (((1 << card_roll_mod33) & 1912611214) == 0);
-		is_reject |= is_rare && (((1 << card_roll_mod19) & 67120) == 0);
+		bool is_reject = (is_common) && (((1 << card_roll_modA) & BAD_COMMON) == 0);
+		is_reject |= is_uncommon && (((1 << card_roll_modB) & BAD_UNCOMMON) == 0);
+		is_reject |= is_rare && (((1 << card_roll_modA) & BAD_RARE) == 0);
 
 		if (is_reject) {
 			return false;
 		}
 
-		uint8 card1 = (is_rare) ? (card_roll_mod19 + 52) : card_roll_mod19;
-		card1 = (is_uncommon) ? (card_roll_mod33 + 19) : card1;
+		uint8 card1 = (is_rare) ? (card_roll_modA + NUM_AB) : card_roll_modA;
+		card1 = (is_uncommon) ? (card_roll_modB + NUM_A) : card1;
 
 		foundrare = is_rare;
 		adj = (is_common) ? (adj - 1) : adj;
 		adj = (is_rare) ? 5 : adj;
 
-		/*
-		uint8 card1;
-
-		//rare
-		if (roll < 3) {
-			card1 = random64Fast<19>(seed0, seed1);
-			if ((1 << card1) & 67120) {
-				adj = 5;
-				foundrare = true;
-				card1 += 52;
-			}
-			else {
-				return false;
-			}
-		}
-
-		//common
-		else if (roll >= 40) {
-			card1 = random64Fast<19>(seed0, seed1);
-			if ((1 << card1) & 188480) {
-				adj -= 1;
-			}
-			else {
-				return false;
-			}
-		}
-
-		//uncommon
-		else {
-			card1 = random64Fast<33>(seed0, seed1);
-			if ((1 << card1) & 1912611214) {
-				card1 += 19;
-			}
-			else {
-				return false;
-			}
-		}
-		*/
-
 		//second card
-
 		roll = random64Fast<100>(seed0, seed1) + adj;
 		uint8 card2;
 
@@ -350,25 +301,20 @@ __forceinline__ __device__ bool testBadSilentCardsFast(const uint64 seed) {
 		is_uncommon = (!is_common) && (!is_rare);
 
 		do {
-			card_roll = random64Fast<627>(seed0, seed1);
-			card_roll_mod19 = card_roll % 19;
-			card_roll_mod33 = card_roll % 33;
+			card_roll = random64Fast<NUM_GCD>(seed0, seed1);
+			card_roll_modA = card_roll % NUM_A;
+			card_roll_modB = card_roll % NUM_B;
 
-			is_reject = is_common && (((1 << card_roll_mod19) & 188480) == 0);
-			is_reject |= is_uncommon && (((1 << card_roll_mod33) & 1912611214) == 0);
-			is_reject |= is_rare && (((1 << card_roll_mod19) & 67120) == 0);
+			is_reject = is_common && (((1 << card_roll_modA) & BAD_COMMON) == 0);
+			is_reject |= is_uncommon && (((1 << card_roll_modB) & BAD_UNCOMMON) == 0);
+			is_reject |= is_rare && (((1 << card_roll_modA) & BAD_RARE) == 0);
 
 			if (is_reject) {
 				return false;
 			}
 
-			//card2 = card_roll_mod19 + ((uint8)is_rare) * 52;
-			//card2 += ((uint8)is_uncommon) * (card_roll_mod33 + 19 - card2);
-
-
-
-			card2 = (is_rare) ? (card_roll_mod19 + 52) : card_roll_mod19;
-			card2 = (is_uncommon) ? (card_roll_mod33 + 19) : card2;
+			card2 = (is_rare) ? (card_roll_modA + NUM_AB) : card_roll_modA;
+			card2 = (is_uncommon) ? (card_roll_modB + NUM_A) : card2;
 
 
 		} while (card2 == card1);
@@ -376,60 +322,6 @@ __forceinline__ __device__ bool testBadSilentCardsFast(const uint64 seed) {
 		adj = (is_common) ? (adj - 1) : adj;
 		adj = (is_rare) ? 5 : adj;
 		foundrare |= is_rare;
-
-		/*
-		//rare
-		if (roll < 3) {
-
-			while (card2 == card1) {
-
-				card2 = random64Fast<19>(seed0, seed1);
-
-				if ((1 << card2) & 67120) {
-					card2 += 52;
-				}
-				else {
-					return false;
-				}
-			}
-
-			adj = 5;
-			foundrare = true;
-		}
-
-		//common
-		else if (roll >= 40) {
-
-			while (card2 == card1) {
-
-				card2 = random64Fast<19>(seed0, seed1);
-
-				if ((1 << card2) & 188480) {}
-				else {
-					return false;
-				}
-			}
-
-			adj -= 1;
-		}
-
-		//uncommon
-		else {
-
-			while (card2 == card1) {
-
-				card2 = random64Fast<33>(seed0, seed1);
-
-				if ((1 << card2) & 1912611214) {
-					card2 += 19;
-				}
-				else {
-					return false;
-				}
-			}
-		}
-
-		*/
 
 		//third card
 		roll = random64Fast<100>(seed0, seed1) + adj;
@@ -440,88 +332,26 @@ __forceinline__ __device__ bool testBadSilentCardsFast(const uint64 seed) {
 		is_uncommon = (!is_common) && (!is_rare);
 
 		do {
-			card_roll = random64Fast<627>(seed0, seed1);
-			card_roll_mod19 = card_roll % 19;
-			card_roll_mod33 = card_roll % 33;
+			card_roll = random64Fast<NUM_GCD>(seed0, seed1);
+			card_roll_modA = card_roll % NUM_A;
+			card_roll_modB = card_roll % NUM_B;
 
-			is_reject = is_common && (((1 << card_roll_mod19) & 188480) == 0);
-			is_reject |= is_uncommon && (((1 << card_roll_mod33) & 1912611214) == 0);
-			is_reject |= is_rare && (((1 << card_roll_mod19) & 67120) == 0);
+			is_reject = is_common && (((1 << card_roll_modA) & BAD_COMMON) == 0);
+			is_reject |= is_uncommon && (((1 << card_roll_modB) & BAD_UNCOMMON) == 0);
+			is_reject |= is_rare && (((1 << card_roll_modA) & BAD_RARE) == 0);
 
 			if (is_reject) {
 				return false;
 			}
 
-			card3 = (is_rare) ? (card_roll_mod19 + 52) : card_roll_mod19;
-			card3 = (is_uncommon) ? (card_roll_mod33 + 19) : card3;
+			card3 = (is_rare) ? (card_roll_modA + NUM_AB) : card_roll_modA;
+			card3 = (is_uncommon) ? (card_roll_modB + NUM_A) : card3;
 		} while ((card3 == card1) || (card3 == card2));
 
 		foundrare |= is_rare;
 		adj = (is_common) ? (adj - 1) : adj;
 		adj = (is_rare) ? 5 : adj;
 
-
-		/*
-		if (i == 1) {
-			if ((roll != 24) || seed != 1) {
-			return false;
-			}
-		}
-		*/
-
-		/*
-		uint8 card3 = card1;
-		//rare
-		if (roll < 3) {
-
-			while ((card3 == card1) || (card3 == card2)) {
-
-				card3 = random64Fast<19>(seed0, seed1);
-
-				if ((1 << card3) & 67120) {
-					card3 += 52;
-				}
-				else {
-					return false;
-				}
-			}
-
-			adj = 5;
-			foundrare = true;
-		}
-
-		//common
-		else if (roll >= 40) {
-
-			while ((card3 == card1) || (card3 == card2)) {
-
-				card3 = random64Fast<19>(seed0, seed1);
-
-				if ((1 << card3) & 188480) {}
-				else {
-					return false;
-				}
-			}
-
-			adj -= 1;
-		}
-
-		//uncommon
-		else {
-
-			while ((card3 == card1) || (card3 == card2)) {
-
-				card3 = random64Fast<33>(seed0, seed1);
-
-				if ((1 << card3) & 1912611214) {
-					card3 += 19;
-				}
-				else {
-					return false;
-				}
-			}
-		}
-		*/
 
 		if (foundrare) {
 			random64Fast<2>(seed0, seed1);
@@ -532,41 +362,6 @@ __forceinline__ __device__ bool testBadSilentCardsFast(const uint64 seed) {
 			random64Fast<2>(seed0, seed1);
 			random64Fast<2>(seed0, seed1);
 		}
-
-		/*
-		if (i == 1) {
-			if ((card1 != 5) || (card2 != 1) || (card3 != 46)) {
-				return false;
-			}
-		}
-		if (i == 2) {
-			if ((card1 != 40) || (card2 != 45) || (card3 != 50)) {
-			//if ((card1 != 27) || (card2 != 45) || (card3 != 50)) {
-					return false;
-			}
-		}
-
-		if (i == 3) {
-			if ((card1 != 20) || (card2 != 23) || (card3 != 25)) {
-				return false;
-			}
-		}
-		if (i == 4) {
-			if ((card1 != 33) || (card2 != 4) || (card3 != 5)) {
-				return false;
-			}
-		}
-		if (i == 5) {
-			if ((card1 != 48) || (card2 != 34) || (card3 != 9)) {
-				return false;
-			}
-
-			else {
-				return true;
-			}
-		}
-		*/
-
 	}
 
 	return true;
@@ -664,7 +459,7 @@ __global__ void badSilentKernel(TestInfo info, uint64* results) {
 	results[totalIdx] = false;
 	for (; seed < info.end; seed += info.blocks * info.threads)
 	{
-		if (testBadNeow<nCardRewards>(seed)) {
+		if (testBadNeow1<nCardRewards>(seed)) {
 			if (testBadSilentCardsFast<nCardRewards>(seed)) {
 				results[totalIdx] = seed;
 				return;
@@ -681,7 +476,7 @@ __global__ void badIroncladKernel(TestInfo info, uint64* results) {
 	results[totalIdx] = false;
 	for (; seed < info.end; seed += info.blocks * info.threads)
 	{
-		if (testBadNeow<nCardRewards>(seed)) {
+		if (testBadNeow1<nCardRewards>(seed)) {
 			if (testCombatQNodes<3>(seed)) {
 				results[totalIdx] = seed;
 				return;
