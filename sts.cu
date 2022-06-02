78c0ad923d648d02b7832ef6584971a2c9e9f0f3
OohBleh <76603321+OohBleh@users.noreply.github.com>
2022-06-02 11:08:40 -0400

    new shard speedrun stuff

--- sts.cu
+++ sts.cu
@@ -89,6 +89,18 @@ __forceinline__ __device__ uint64 randomPreFloatFast(uint64& seed0, uint64& seed
 	return x;
 }
 
+__forceinline__ __device__ uint64 randomLong(uint64& seed0, uint64& seed1) {
+
+	uint64 s1 = seed0;
+	uint64 s0 = seed1;
+	seed0 = s0;
+	s1 ^= s1 << 23;
+	seed1 = s1 ^ s0 ^ s1 >> 17 ^ s0 >> 26;
+
+	uint64 x = (seed0 + seed1);
+	return x;
+}
+
 // ************************************************************** EMD RNG Functions
 
 // ************************************************************** BEGIN Accurate Pandora Functions
@@ -1608,9 +1620,6 @@ __forceinline__ __device__ bool testSealedB(const uint64 seed) {
 	return (ctrs[0] > 0) && (ctrs[1] > 0);
 }
 
-
-
-
 __global__ void SealedRBKernel(TestInfo info, uint64* results) {
 	const unsigned int totalIdx = blockIdx.x * info.threads + threadIdx.x;
 	uint64 seed = info.start + static_cast<uint64>(totalIdx);
@@ -1643,7 +1652,291 @@ __global__ void SealedBKernel(TestInfo info, uint64* results) {
 	}
 }
 
-// ************************************************************** BEGIN Custom Mode Functions
+// ************************************************************** END Custom Mode Functions
+
+// ************************************************************** BEGIN Shuffle Functions
+
+static constexpr uint64 JAVA_MULTIPLIER = 0x5DEECE66DULL;
+static constexpr uint64 JAVA_ADDEND = 0xBULL;
+static constexpr uint64 JAVA_MASK = (1ULL << 48) - 1;
+
+__forceinline__ __device__ bool shardFirst(const uint64 seed) {
+
+	uint64 seed0 = murmurHash3(seed);
+	uint64 seed1 = murmurHash3(seed0);
+
+	// shuffle commons, uncommons, rares
+	randomLong(seed0, seed1);
+	randomLong(seed0, seed1);
+	randomLong(seed0, seed1);
+	
+	uint64 shuffleSeed = randomLong(seed0, seed1);
+	shuffleSeed = (shuffleSeed ^ JAVA_MULTIPLIER) & JAVA_MASK;
+	shuffleSeed = (shuffleSeed * JAVA_MULTIPLIER + JAVA_ADDEND) & JAVA_MASK;
+	//int32 r = static_cast<int32>(shuffleSeed >> (48 - 31));
+	int32 r = static_cast<int32>(shuffleSeed >> 17);
+	//static constexpr int bound = 16;
+	//r = static_cast<int32>(((bound * static_cast<uint64>(r)) >> 31));
+	r = static_cast<int32>(((16 * static_cast<uint64>(r)) >> 31));
+	
+	return r == 8;
+}
+
+
+
+__forceinline__ __device__ bool startsPBox(uint64_t seed) {
+	uint64 seed0 = murmurHash3(seed);
+	uint64 seed1 = murmurHash3(seed0);
+	// shuffle commons, uncommons, rares, shops
+	randomLong(seed0, seed1);
+	randomLong(seed0, seed1);
+	randomLong(seed0, seed1);
+	randomLong(seed0, seed1);
+
+	uint64 shuffleSeed = randomLong(seed0, seed1);
+	seed = (seed ^ JAVA_MULTIPLIER) & JAVA_MASK;
+
+	int size = 22;
+	int K = 5;
+	for (int i = size; i > 1; i--) {
+
+		seed = (seed * JAVA_MULTIPLIER + JAVA_ADDEND) & JAVA_MASK;
+		int r = static_cast<std::int32_t>(seed >> (48 - 31));
+		int bound = i;
+		int m = bound - 1;
+		if ((bound & m) == 0)
+			r = static_cast<int32_t>(((bound * static_cast<std::uint64_t>(r)) >> 31));
+		else {
+			for (int32_t u = r; u - (r = u % bound) + m < 0; ) {
+				seed = (seed * JAVA_MULTIPLIER + JAVA_ADDEND) & JAVA_MASK;
+				u = static_cast<std::int32_t>(seed >> (48 - 31));
+			}
+		}
+
+		if (5 == r) {
+			return false;
+		}
+
+		else if (K == r) {
+			K = i - 1;
+		}
+		else if (K == i - 1) {
+			K = r;
+		}
+	}
+
+	return K < 3;
+
+}
+
+
+__forceinline__ __device__ bool hyperbeamFirstShop(const uint64 seed) {
+
+	constexpr uint8 D_NUM_A = 18;
+	constexpr uint8 D_NUM_B = 36;
+	constexpr uint8 D_NUM_C = 17;
+
+	uint64 seed0 = murmurHash3(seed);
+	uint64 seed1 = murmurHash3(seed0);
+
+	//first card
+	int8 roll = random64Fast<100>(seed0, seed1);
+	bool is_common = (roll >= 35);
+
+	uint16 card1 = (is_common) ? random64Fast<D_NUM_A>(seed0, seed1) : (random64Fast<D_NUM_B>(seed0, seed1) + D_NUM_A);
+
+	int8 adj = (is_common) ? (4) : (5);
+
+	//second card
+	roll = random64Fast<100>(seed0, seed1) + adj;
+	is_common = (roll >= 40);
+
+	uint16 card2;
+	do {
+		card2 = (is_common) ? random64Fast<D_NUM_A>(seed0, seed1) : (random64Fast<D_NUM_B>(seed0, seed1) + D_NUM_A);
+	} while (card2 == card1);
+
+	adj = (is_common) ? (adj - 1) : adj;
+
+	//third card
+	roll = random64Fast<100>(seed0, seed1) + adj;
+	is_common = (roll >= 40);
+
+	uint16 card3;
+	do {
+		card3 = (is_common) ? random64Fast<D_NUM_A>(seed0, seed1) : (random64Fast<D_NUM_B>(seed0, seed1) + D_NUM_A);
+	} while ((card3 == card1) || (card3 == card2));
+
+	random64Fast<2>(seed0, seed1);
+	random64Fast<2>(seed0, seed1);
+	random64Fast<2>(seed0, seed1);
+
+	adj = (is_common) ? (adj - 1) : adj;
+	
+	// first shop attack
+	// rarity
+	roll = random64Fast<100>(seed0, seed1) + adj;
+
+	if (roll < 9) {
+		// random rare attack
+		card1 = random64Fast<5>(seed0, seed1);
+		/*
+		if (card1 == 2) {
+			return true;
+		}
+		*/
+	}
+	else if (roll >= 46) {
+		// random common attack
+		card1 = 5 + random64Fast<10>(seed0, seed1);
+	}
+	else {
+		// random uncommon attack
+		card1 = 15 + random64Fast<8>(seed0, seed1);
+	}
+
+	// second shop attack
+	do {
+		roll = random64Fast<100>(seed0, seed1) + adj;
+		if (roll < 9) {
+			// random rare attack
+			card2 = random64Fast<5>(seed0, seed1);
+			/*
+			if (card2 == 2) {
+				return true;
+			}
+			*/
+		}
+		else if (roll >= 46) {
+			// random common attack
+			card2 = 5 + random64Fast<10>(seed0, seed1);
+		}
+		else {
+			// random uncommon attack
+			card2 = 15 + random64Fast<8>(seed0, seed1);
+		}
+	} while (card2 == card1);
+	
+	if (card1 != 2 && card2 != 2) {
+		return false;
+	};
+
+	// hyperbeam is card 1 (0) or card 2 (1) in the shop
+	uint8 hyperbeamPosition = (card1 == 2) ? 0 : 1;
+
+
+	// first shop skill
+	// rarity
+	roll = random64Fast<100>(seed0, seed1) + adj;
+
+	if (roll < 9) {
+		// random rare skill
+		card1 = random64Fast<6>(seed0, seed1);
+		/*
+		if (card1 == 2) {
+			return true;
+		}
+		*/
+	}
+	else if (roll >= 46) {
+		// random common skill
+		card1 = 6 + random64Fast<8>(seed0, seed1);
+	}
+	else {
+		// random uncommon skill
+		card1 = 14 + random64Fast<19>(seed0, seed1);
+	}
+
+	// second shop skill
+	do {
+		roll = random64Fast<100>(seed0, seed1) + adj;
+		if (roll < 9) {
+			// random rare skill
+			card2 = random64Fast<6>(seed0, seed1);
+			/*
+			if (card2 == 2) {
+				return true;
+			}
+			*/
+		}
+		else if (roll >= 46) {
+			// random common skill
+			card2 = 6 + random64Fast<8>(seed0, seed1);
+		}
+		else {
+			// random uncommon skill
+			card2 = 14 + random64Fast<19>(seed0, seed1);
+		}
+	} while (card2 == card1);
+
+	// power card (rarity & card)
+	random64Fast<2>(seed0, seed1);
+	random64Fast<2>(seed0, seed1);
+
+	// colorless cards (uncommon and rare)
+	random64Fast<2>(seed0, seed1);
+	random64Fast<2>(seed0, seed1);
+	
+	// do some stuff to see if Blasphemy is generated at Act I boss reward
+	// dud rarity roll
+	// random64Fast<2>(seed0, seed1);
+	
+	// rare Shard card roll
+	if (random64Fast<84>(seed0, seed1) != 56) {
+		//return false;
+	}
+
+	// is Liars Game first, second, or third event? & 
+	// is Hyperbeam discounted?
+	seed0 = murmurHash3(seed);
+	seed1 = murmurHash3(seed0);
+	
+	// are the first two ?-nodes events?
+	if (
+		randomFloatFast(seed0, seed1) < 0.15f || 
+		randomFloatFast(seed0, seed1) < 0.30f
+	) {
+		return false;
+	}
+	
+	// 3rd, 4th, and 5th RNG calls coincide with 1st, 2nd, and 3rd event room
+	static constexpr bool SERPENT_MISSED[9][8][7] = { true,true,true,false,true,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,false,false,false,false,false,false,false,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,false,false,false,false,false,false,false,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,false,false,false,false,false,false,false,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,false,false,false,false,false,false,false,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,false,false,false,false,false,false,false,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,false,false,false,false,false,false,false,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,false,false,false,false,false,false,false,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,false,false,false,false,false,false,false,true,true,true,true,true,false,true,true,true,true,true,true,false,true };
+	if (SERPENT_MISSED[random64Fast<9>(seed0, seed1)][random64Fast<8>(seed0, seed1)][random64Fast<7>(seed0, seed1)]) {
+		return false;
+	}
+
+	random64Fast<2>(seed0, seed1);
+	random64Fast<2>(seed0, seed1);
+
+
+	// 7th RNG call coincides with position of discounted card
+	return random64Fast<5>(seed0, seed1) == hyperbeamPosition;
+}
+
+
+
+__global__ void shardKernel(TestInfo info, uint64* results) {
+	const unsigned int totalIdx = blockIdx.x * info.threads + threadIdx.x;
+	uint64 seed = info.start + static_cast<uint64>(totalIdx);
+
+	results[totalIdx] = false;
+	for (; seed < info.end; seed += info.blocks * info.threads)
+	{
+		if (
+			neowsLament(seed) 
+			&& shardFirst(seed) 
+			&& hyperbeamFirstShop(seed)
+			&& startsPBox(seed)
+		) {
+			results[totalIdx] = seed;
+			return;
+		}
+	}
+}
+
+
+
+// ************************************************************** END Shuffle Functions
 
 
 
@@ -1703,6 +1996,10 @@ cudaError_t testPandoraSeedsWithCuda(TestInfo info, FunctionType fnc, uint64* re
 
 	case FunctionType::CUSTOM:
 		SealedRBKernel << <info.blocks, info.threads >> > (info, dev_results);
+		break;
+
+	case FunctionType::SHARD:
+		shardKernel << <info.blocks, info.threads >> > (info, dev_results);
 		break;
 
 	default:
--- sts.cuh
+++ sts.cuh
@@ -9,8 +9,13 @@
 
 typedef std::int8_t int8;
 typedef std::uint8_t uint8;
+
 typedef std::int16_t int16;
 typedef std::uint16_t uint16;
+
+typedef std::int32_t int32;
+typedef std::uint32_t uint32;
+
 typedef std::uint64_t uint64;
 typedef std::int64_t int64;
 
@@ -22,7 +27,8 @@ enum class FunctionType {
 	BAD_IRONCLAD,
 	BAD_MAP,
 	FAST_QNODES, 
-	CUSTOM
+	CUSTOM, 
+	SHARD
 };
 
 const std::int8_t searchLength = 5;
